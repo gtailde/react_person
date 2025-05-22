e872f77420f6a07b3e103aa2c72fba980a636034
Oleg Nepiuk <gtailde@gmail.com>
2025-05-22 07:32:08 +0200

    fix person component

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -4,7 +4,7 @@ export const Person = ({ person }) => (
     {person.age && <p className="Person__age">I am {person.age}</p>}
     {person.isMarried ? (
       <p className="Person__partner">
-        {person.partnerName} is my {person.sex === 'm' ? 'wife' : 'husband'}
+        {person.partnerName} is my {person.sex === 'f' ? 'husband' : 'wife'}
       </p>
     ) : (
       <p className="Person__partner">I am not married</p>
